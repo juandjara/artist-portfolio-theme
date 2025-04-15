22780de1f7605caa3ef2d17ac83920667cfe3061
Juan D. Jara <juanorigami@gmail.com>
2025-04-15 17:44:21 +0200

    deploy configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,13 +8,18 @@ import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import { i18nConfig } from "./src/lib/i18n";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://nesgarciaart.netlify.app",
   integrations: [preact({ devtools: true }), icon()],
+
   vite: {
     // @ts-ignore
     plugins: [tailwindcss()],
   },
+
   i18n: i18nConfig,
+  adapter: netlify(),
 });
