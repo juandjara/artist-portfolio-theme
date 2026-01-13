9dd056ce2cbbdc29ecc3ca1a5abec43bbdb794d6
Juan D. Jara <juanorigami@gmail.com>
2026-01-13 14:55:59 +0100

    fix all icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
-import preact from "@astrojs/preact";
-
-import tailwindcss from "@tailwindcss/vite";
-
-import icon from "astro-icon";
-import { i18nConfig } from "./src/lib/i18n";
-
-import netlify from "@astrojs/netlify";
+import { defineConfig } from "astro/config"
+import preact from "@astrojs/preact"
+import tailwindcss from "@tailwindcss/vite"
+import { i18nConfig } from "./src/lib/i18n"
+import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nesgarciaart.netlify.app",
-  integrations: [preact({ devtools: true }), icon()],
+  integrations: [preact({ devtools: true })],
 
   vite: {
     // @ts-ignore
@@ -25,4 +20,4 @@ export default defineConfig({
   adapter: netlify({
     // edgeMiddleware: true
   }),
-});
+})
